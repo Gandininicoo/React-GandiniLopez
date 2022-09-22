c07d8d21fa0948d35d3e1f000da0789500283147
Nicolas Gandini Lopez <nicolasgandinilopez@gmail.com>
2022-09-22 19:45:39 -0300

    primer pre entrega

---
+++ src/components/ProductList.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ProductList = () => {
+    return (
+        <>
+            <section class="navContenido">
+                <h3>PERFUMERIA</h3>
+                <h3>BEBES</h3>
+                <h3>CUIDADO PERSONAL</h3>
+                <h3>DERMO COSMETICA</h3>
+                <h3>ANALGESICOS VENTA LIBRE</h3>
+            </section>
+        </>
+    )
+}
+
+export default ProductList
